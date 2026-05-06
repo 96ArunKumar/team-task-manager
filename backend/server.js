4143ffb46fbbecea65f31e3617c95f419d143a58
96ArunKumar <arunchauhan72746@gamil.com>
2026-05-06 11:58:03 +0530

    fix frontend routing

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -5,6 +6,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend")));
 app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI)
@@ -16,4 +18,9 @@ app.use("/api/projects", require("./routes/project"));
 app.use("/api/tasks", require("./routes/task"));
 app.use("/api/dashboard", require("./routes/dashboard"));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
 app.listen(5000, ()=>console.log("Server running on 5000"));
+
