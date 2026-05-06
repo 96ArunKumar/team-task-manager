238cbfbdb46f34afc63ea1ed28a9edf0f72a3395
96ArunKumar <arunchauhan72746@gamil.com>
2026-05-07 00:17:08 +0530

    fixed mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -5,26 +5,45 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "../frontend")));
+
 app.use(cors());
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("DB Connected"))
-.catch(err=>console.log(err));
+app.use(express.static(path.join(__dirname, "../frontend")));
 
+// ROUTES
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/projects", require("./routes/project"));
 app.use("/api/tasks", require("./routes/task"));
 app.use("/api/dashboard", require("./routes/dashboard"));
 
+// FRONTEND
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
 
+// DATABASE CONNECTION + SERVER START
+async function startServer() {
+
+  try {
+
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Connected ✅");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+
+  } catch (err) {
+
+    console.log("MongoDB Error ❌");
+    console.log(err);
+
+  }
+}
+
+startServer();
